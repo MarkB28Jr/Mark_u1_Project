d6b32eea2d50163ca13ad55c14e7a009f48780bf
MarkB28Jr <mark.brashear265@gmail.com>
2024-03-11 15:29:08 -0500

    Completed CSS Cards / HTML // Beginning of JS

--- main.js
+++ main.js
@@ -1,70 +1,36 @@
 // cache
-const boardEl = document.getElementsByClassName('gameBoard');
-const cardsEl = document.getElementsByClassName('cards');
+const cardsEl = document.querySelectorAll('.cards');
 const titleEl = document.querySelector('h1');
 const playerStatus = document.querySelector('p');
 
-let currentPlayer = 'Bob';
+let flippedCard = false;
+let lockBoard = false;
+let currentPlayer = '';
 let score = 0;
+let firstCard, secondCard;
 
 playerStatus.innerHTML = `<u>Score</u> <br> ${currentPlayer}: ${score}`;
 console.log(playerStatus);
 
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.toggle('flip');
+
+    if (!flippedCard) {
+        firstCard = this;
+        flippedCard = true;
+        return;
+    }
+    secondCard = this;
+    checkIfMatched();
+}
+
+cardsEl.forEach(card => card.addEventListener('click', flipCard));
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// functions
-
-// function init() {
-
-// }
-
-// function render() {
-
-// }
-
-// function message() {
-
-// }
-
-// function startPage() {
-    
-// }
-
-// function restartPage() {
-    
-// }
-
-// function randomizeCards() {
-
-// }
-
-// function winningCombos() {
-
-// }
-
-// function boxClicked() {
-
-// }
-
-// event listners
 
